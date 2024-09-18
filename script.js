2e936c02eef9a666648a81d37939c0fd0ea08448
madzga <madzgarashvililasha98@gmail.com>
2024-09-18 18:43:41 +0400

    lesson1 offline

---
+++ script.js
@@ -0,0 +1,23 @@
+let numberitem= 21; //number
+
+if (numberitem < 50) {
+    console.log('< 50')
+}   else if (numberitem > 20){
+    console.log('> 20')
+}
+
+let nameperson = 'mariam' //name
+
+if (nameperson = 'mariam'){
+    console.log(true)
+} 
+
+let nameitem = 'mariami'
+
+switch (nameitem) {
+    case mari : 
+    alert('wrong')
+
+    default: 
+    alert ('wrong')
+}
